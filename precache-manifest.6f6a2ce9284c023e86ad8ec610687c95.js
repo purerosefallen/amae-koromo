42bd1ece2027381274e4eb15d019337ff6bc81ed
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-05 00:36:21 +0000

    Deploy SAPikachu/amae-koromo to github.com/SAPikachu/amae-koromo.git:gh-pages

---
+++ precache-manifest.6f6a2ce9284c023e86ad8ec610687c95.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6012a8aaee873e42f66b6f0fe9d42a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9d1baed467fcb0463c1",
+    "url": "/static/css/main.de4a6b1a.chunk.css"
+  },
+  {
+    "revision": "a72305ecbc020a2228e8",
+    "url": "/static/js/0.dfb4ac81.chunk.js"
+  },
+  {
+    "revision": "bdd7738b33a104acef8b",
+    "url": "/static/js/1.5f65ef90.chunk.js"
+  },
+  {
+    "revision": "88968d2a12fe0507b89c",
+    "url": "/static/js/10.64e556d5.chunk.js"
+  },
+  {
+    "revision": "1c49b0465c9943c14638",
+    "url": "/static/js/2.225e4734.chunk.js"
+  },
+  {
+    "revision": "d07c58954da64488a125",
+    "url": "/static/js/5.b2f0ec80.chunk.js"
+  },
+  {
+    "revision": "9af2b307866353d17125",
+    "url": "/static/js/6.3e47e455.chunk.js"
+  },
+  {
+    "revision": "7c40b06b806be0e913f8",
+    "url": "/static/js/7.1276bdf2.chunk.js"
+  },
+  {
+    "revision": "02ad292c5e7969c55dc5",
+    "url": "/static/js/8.bfa1b428.chunk.js"
+  },
+  {
+    "revision": "e00d58fbdd867bf111b6",
+    "url": "/static/js/9.8a608fc0.chunk.js"
+  },
+  {
+    "revision": "a9d1baed467fcb0463c1",
+    "url": "/static/js/main.f4b3b520.chunk.js"
+  },
+  {
+    "revision": "889c5fbf7b142b10bd12",
+    "url": "/static/js/runtime-main.5af27ebf.js"
+  },
+  {
+    "revision": "eed9285712949ad4252edb9f008eefe6",
+    "url": "/static/media/koromo.preload.eed92857.jpg"
+  }
+]);
